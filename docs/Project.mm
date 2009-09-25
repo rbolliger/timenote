70eab84842944958e91be926db49a1fea0a46a73
rbo@donax.ch <rbo@donax.ch@97fa114c-a42c-11de-ba6e-69e41c8db662>
2009-09-25 19:13:52 +0000

    Added mind-map representation of Project table

---
+++ docs/Project.mm
@@ -0,0 +1,30 @@
+<map version="0.7.1">
+<node TEXT="Project">
+<node TEXT="Activity" POSITION="right">
+<node TEXT="Working">
+<node TEXT="Meeting"/>
+<node TEXT="Programming"/>
+<node TEXT="holidays"/>
+<node TEXT="funeral"/>
+<node TEXT="..."/>
+</node>
+<node TEXT="Non-working">
+<node TEXT="dentist"/>
+<node TEXT="shopping"/>
+<node TEXT="..."/>
+</node>
+</node>
+<node TEXT="Behaviors" POSITION="left">
+<node TEXT="Max Days/horus/% Total"/>
+<node TEXT="Max Days/horus/% consecutive"/>
+<node TEXT="max occurences"/>
+<node TEXT="..."/>
+</node>
+<node TEXT="Authorizations" POSITION="right">
+<node TEXT="Admin: manage properties, CRUD"/>
+<node TEXT="Manager: create sub-projects"/>
+<node TEXT="Worker: post hours"/>
+<node TEXT="Reader: can see but not edit"/>
+</node>
+</node>
+</map>
