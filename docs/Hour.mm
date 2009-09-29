e92265c43b1e08497c8c9e4675e2d6b5c3d38893
rbo@donax.ch <rbo@donax.ch@97fa114c-a42c-11de-ba6e-69e41c8db662>
2009-09-29 17:18:37 +0000

    Added new mindmap for Hour table

---
+++ docs/Hour.mm
@@ -0,0 +1,19 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1254243823518" ID="Freemind_Link_882581330" MODIFIED="1254243828834" TEXT="Hour">
+<node CREATED="1254243838976" ID="_" MODIFIED="1254243884064" POSITION="right" TEXT="User Table">
+<icon BUILTIN="forward"/>
+</node>
+<node CREATED="1254243887622" ID="Freemind_Link_1864879242" MODIFIED="1254243936964" POSITION="left" TEXT="Project Table">
+<icon BUILTIN="back"/>
+</node>
+<node CREATED="1254243946990" ID="Freemind_Link_347367741" MODIFIED="1254243954384" POSITION="right" TEXT="Categories">
+<node CREATED="1254243962660" ID="Freemind_Link_782195073" MODIFIED="1254243968000" TEXT="Meeting"/>
+<node CREATED="1254243968372" ID="Freemind_Link_1037696826" MODIFIED="1254243970736" TEXT="Coding"/>
+<node CREATED="1254243971844" ID="Freemind_Link_1843983007" MODIFIED="1254243976272" TEXT="Reporting"/>
+<node CREATED="1254243976532" ID="Freemind_Link_1415081012" MODIFIED="1254243983840" TEXT="Documentation"/>
+<node CREATED="1254243984372" ID="Freemind_Link_1259284931" MODIFIED="1254243993200" TEXT="Literature review"/>
+<node CREATED="1254243993956" ID="Freemind_Link_967673837" MODIFIED="1254243995520" TEXT="..."/>
+</node>
+</node>
+</map>
