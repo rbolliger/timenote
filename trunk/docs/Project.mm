d8d16a7ca136b5adc1433d77728806102a224937
rbo@donax.ch <rbo@donax.ch@97fa114c-a42c-11de-ba6e-69e41c8db662>
2009-09-29 17:18:37 +0000

    Added new mindmap for Hour table

--- trunk/docs/Project.mm
+++ trunk/docs/Project.mm
@@ -1,30 +1,31 @@
-<map version="0.7.1">
-<node TEXT="Project">
-<node TEXT="Activity" POSITION="right">
-<node TEXT="Working">
-<node TEXT="Meeting"/>
-<node TEXT="Programming"/>
-<node TEXT="holidays"/>
-<node TEXT="funeral"/>
-<node TEXT="..."/>
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1254243700380" ID="Freemind_Link_390739308" MODIFIED="1254243700380" TEXT="Project">
+<node CREATED="1254243700380" MODIFIED="1254243700380" POSITION="right" TEXT="Activity">
+<node CREATED="1254243700380" ID="Freemind_Link_1596357921" MODIFIED="1254243717600" TEXT="Accounted">
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="Meeting"/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="Programming"/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="holidays"/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="funeral"/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="..."/>
 </node>
-<node TEXT="Non-working">
-<node TEXT="dentist"/>
-<node TEXT="shopping"/>
-<node TEXT="..."/>
+<node CREATED="1254243700380" ID="Freemind_Link_1789723904" MODIFIED="1254243726527" TEXT="Not Accounted">
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="dentist"/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="shopping"/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="..."/>
 </node>
 </node>
-<node TEXT="Behaviors" POSITION="left">
-<node TEXT="Max Days/horus/% Total"/>
-<node TEXT="Max Days/horus/% consecutive"/>
-<node TEXT="max occurences"/>
-<node TEXT="..."/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" POSITION="left" TEXT="Behaviors">
+<node CREATED="1254243700380" ID="Freemind_Link_1696283449" MODIFIED="1254243744815" TEXT="Max Days/hours/% Total"/>
+<node CREATED="1254243700380" ID="Freemind_Link_1219351065" MODIFIED="1254243740625" TEXT="Max Days/hours/% consecutive"/>
+<node CREATED="1254243700380" MODIFIED="1254243700380" TEXT="max occurences"/>
+<node CREATED="1254243700381" MODIFIED="1254243700381" TEXT="..."/>
 </node>
-<node TEXT="Authorizations" POSITION="right">
-<node TEXT="Admin: manage properties, CRUD"/>
-<node TEXT="Manager: create sub-projects"/>
-<node TEXT="Worker: post hours"/>
-<node TEXT="Reader: can see but not edit"/>
+<node CREATED="1254243700381" MODIFIED="1254243700381" POSITION="right" TEXT="Authorizations">
+<node CREATED="1254243700381" MODIFIED="1254243700381" TEXT="Admin: manage properties, CRUD"/>
+<node CREATED="1254243700381" MODIFIED="1254243700381" TEXT="Manager: create sub-projects"/>
+<node CREATED="1254243700381" MODIFIED="1254243700381" TEXT="Worker: post hours"/>
+<node CREATED="1254243700381" MODIFIED="1254243700381" TEXT="Reader: can see but not edit"/>
 </node>
 </node>
 </map>
