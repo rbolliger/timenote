0e973d8eb024d438ae6723e17004c5340db33fab
ponsfrilus <ponsfrilus@97fa114c-a42c-11de-ba6e-69e41c8db662>
2009-10-10 00:05:26 +0000

    new css (not really finished yet)

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -8,57 +8,53 @@
     <?php include_stylesheets() ?>
   </head>
   <body>
+  <div id="container">
+    <div id="header">
+      <h1>
+        <a href="<?php echo url_for('capture/main') ?>">
+          <img src="/images/logo.jpg" alt="timenote capture board" />
+        
+        <span class="sous-titre">
+          <strong>timenote:</strong>
+          worker's time capture
+        </span></a>
+      </h1>
 
-    <div id="container">
-
-      <div id="header">
-
-        <div class="content">
-
-          <h1>
-            <a href="<?php echo url_for('capture/main') ?>">
-              <img src="/images/logo.jpg" alt="timenote capture board" />
-            </a>
-          </h1>
-
-          <div id="menu">
-            <ul>
-            <?php if ($sf_user->isAuthenticated()) : ?>
-              <li>
-                <?php echo link_to('Capture hours', 'capture/main') ?>
-              </li>
-              <li>
-                <?php echo link_to('Search & Export', 'capture/index') ?>
-              </li>
-              <li>
-                <!-- Spacer --> &nbsp;
-              </li>
-              <li>
-                <?php echo link_to('User Options', '@homepage') ?>
-              </li>
-              <li>
-                <!-- Spacer --> &nbsp;
-              </li>
-              <li>
-                <?php echo link_to('Sign out', '@sf_guard_signout') ?>
-              </li>
+    </div>
+    <div id="menu">
+      <ul>
+        <?php if ($sf_user->isAuthenticated()) : ?>
+          <li>
+            <?php echo link_to('Capture hours', 'capture/main') ?>
+          </li>
+          <li>
+            <?php echo link_to('Search & Export', 'capture/index') ?>
+          </li>
+          <li>
+           <!-- Spacer --> &nbsp;
+          </li>
+          <li>
+            <?php echo link_to('User Options', '@homepage') ?>
+          </li>
+          <li>
+          <!-- Spacer --> &nbsp;
+          </li>
+          <li>
+            <?php echo link_to('Sign out', '@sf_guard_signout') ?>
+          </li>
             <?php else: ?>
-              <li>
-                <?php echo link_to('Login', '@sf_guard_signin') ?>
-              </li>
-              <li>
-                <?php echo link_to('Sign in', '@sf_guard_signin') ?>
-              </li>
-            <?php endif; ?>
-            </ul>
-          </div>
+          <li>
+            <?php echo link_to('Login', '@sf_guard_signin') ?>
+          </li>
+          <li>
+            <?php echo link_to('Sign in', '@sf_guard_signin') ?>
+          </li>
+        <?php endif; ?>
+      </ul>
+    </div>
 
-        </div>
-
-      </div>
-
-      <div id="content">
-
+    <div id="main">
+      <div id="sidebar">
 <!--        <div id="sidebar">
           <?php if (!include_slot('sidebar')): ?>
             <h1>sidebar zone</h1>
@@ -66,8 +62,11 @@
             relative to the main content of the page.</p>
           <?php endif; ?>
         </div> -->
-
-        <div class="content" id="main">
+        <div class="sideblock">sb1</div>
+        <div class="sideblock">sb2</div>
+        <div class="sideblock">sb3</div>
+      </div>
+      <div id="content">
           <?php if ($sf_user->hasFlash('notice')): ?>
             <div class="flash_notice">
               <?php echo $sf_user->getFlash('notice') ?>
@@ -81,12 +80,11 @@
           <?php endif; ?>
 
           <?php echo $sf_content ?>
-        </div>
-      </div>
-
-      <div id="footer">
-         <?php if (has_component_slot('footer')) { include_component_slot('footer'); } ?>
       </div>
     </div>
+  <div id="footer"><?php if (has_component_slot('footer')) { include_component_slot('footer'); } ?></div>
+</div>
+
+
   </body>
 </html>
